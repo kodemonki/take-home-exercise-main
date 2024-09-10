b7f016bc433d9461158a30218a35dd5f926e81ac
Tom Jane <kodemonki@hotmail.com>
2024-09-10 10:29:49 +0100

    Update server.js
    added versioning

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,22 @@
 import cors from "cors";
 import express from "express";
+import data from "../src/js/data.json" assert { type: "json" };
 
 const app = express();
-const port = process.env.SERVER_PORT || 3001 ;
+const port = process.env.SERVER_PORT || 3001;
+const apiVersion = "V1";
 
 /* cors */
 const corsOptions = {
   credentials: true,
-  origin: ['http://localhost:3000', 'http://localhost:1234'],
+  origin: ["http://localhost:3000", "http://localhost:1234"],
 };
 app.use(cors(corsOptions));
 /* cors */
 
-app.get('/', (_, res) => {
-  res.json({ message: 'Hello World' });
-});
+/* routes */
+app.get(`/${apiVersion}/data/`, (req, res) => res.json(data));
+/* routes */
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
